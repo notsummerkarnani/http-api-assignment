0c33e757f41d7085ebd52d86a01e2977048af1d1
notsummerkarnani <93565277+notsummerkarnani@users.noreply.github.com>
2022-02-05 23:52:40 -0500

    Completed

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style': htmlHandler.getStyle,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  '/unauthorized': jsonHandler.unauthorized,
+  '/forbidden': jsonHandler.forbidden,
+  '/internal': jsonHandler.internalError,
+  '/notImplemented': jsonHandler.notImplemented,
+  notFound: jsonHandler.notFound,
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = querystring.parse(parsedUrl.query);
+
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response, params);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1:${port}`);
